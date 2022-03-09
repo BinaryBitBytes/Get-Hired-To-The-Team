060d53e015f68f1ad0aa91b30bf39d0a5834471a
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-03-09 17:37:16 -0500

    added more distinctive flavor to each of the test subjects. Soon i will query and test them all. Muahahahahhaahaha

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const inquirer = require('inquirer');
 //!QUESTIONS
+inquirer.registerPrompt("loop" , require("inquirer-loop")(inquirer));
+
+inquirer.prompt(
+    {
+    type:"loop",
+    name:"Employees",
+    message:"Answer the prompts based on your role that is worthy to the organization",
+    QUESTION:[
+        {
+            type: '__ID__' , 
+            choice:[ __Employee__ , __Engineer__ , __Manager__ , __Intern__],
+            message: "What is your identity in the company?"
+        },
+        {
+            type: '__Name__',
+            name: 'BBB',
+            message: 'What is your name budd-eh?'
+        },
+            ],
+    }
+                );
+
+
+
+
 //ask the user quesitons
 
 // manager office number
@@ -8,15 +33,7 @@ const inquirer = require('inquirer');
 
 // intern has school
 
-
-
-
-
-
-
-
-
-
+// get role()
 
 
 
---
+++ src/test.js
@@ -0,0 +1,23 @@
+const inquirer = require('inquirer');
+//!QUESTIONS
+//inquirer.registerPrompt("loop" , require("inquirer-loop")(inquirer));
+
+inquirer.prompt(
+    {
+    type:"loop",
+    name:"Employees",
+    message:"Answer the prompts based on your role that is worthy to the organization",
+    QUESTION:[
+        {
+            type: '__ID__' , 
+            choice:[ __Employee__ , __Engineer__ , __Manager__ , __Intern__],
+            message: "What is your identity in the company?"
+        },
+        {
+            type: '__Name__',
+            name: 'BBB',
+            message: 'What is your name budd-eh?'
+        },
+            ],
+    }
+                );
