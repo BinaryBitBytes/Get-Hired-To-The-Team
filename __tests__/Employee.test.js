7ce463a5b81f03784bd8faf5c6ab42ec2d86a7aa
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-03-28 17:12:10 -0400

    updated

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,9 @@
 // This is a gucci comment about importing the employee class from the library folder and the file __Employee__.js
-const __Employee__ = require('../lib/__Employee__.js');
 
-import { TestWatcher } from "jest";
-import { } from "./"; // importing a function from a  file
+//const __Employee__ = require('../lib/__Employee__.js');
+
+//import { TestWatcher } from "jest";
+import {__Employee__ } from "'../lib/__Employee__.js'"; // importing a function from a  file
 
 test(//!"write in a statement about a test here, as a arroy of data objects to test.",
     ()=>{
@@ -24,5 +25,7 @@ test(//!"write in a statement about a test here, as a arroy of data objects to t
         //! .toBeTruthy()--> used to verify is true or is false
         //! .not --> verif if another matcher has an opposite result is true
         //! .toContain()--> verify that an item contains an array and that if some items are not in array
-        expect(actualValue).toEqual(expectedOutput)
+        expect(actualValue).toEqual(expectedOutput){
+
+        }
     });
